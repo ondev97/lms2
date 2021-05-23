7d5fa64ee67069f61f46ea446c68ca73b5c5e42a
Navanjane <nkindelpitiya@gmail.com>
2021-05-23 20:32:36 +0530

    course/admin.py edited

--- course/admin.py
+++ course/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Course, Module, Enrollment, Coupon, Subject, ModuleFile,Payment,Zoom
 
 # Register your models here.
-#admin.site.register(Course)
-#admin.site.register(Module)
-#admin.site.register(Enrollment)
-#admin.site.register(Coupon)
-#admin.site.register(Subject)
-#admin.site.register(ModuleFile)
-#admin.site.register(Payment)
-#admin.site.register(Zoom)
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Enrollment)
+admin.site.register(Coupon)
+admin.site.register(Subject)
+admin.site.register(ModuleFile)
+admin.site.register(Payment)
+admin.site.register(Zoom)
