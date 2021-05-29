153188c9440eb79280e0f83da01c8deec29d097c
Navanjane <nkindelpitiya@gmail.com>
2021-05-29 20:12:23 +0530

    enrollments unregistered

--- course/admin.py
+++ course/admin.py
@@ -4,7 +4,7 @@ from .models import Course, Module, Enrollment, Coupon, Subject, ModuleFile,Paym
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(Module)
-admin.site.register(Enrollment)
+#admin.site.register(Enrollment)
 admin.site.register(Coupon)
 admin.site.register(Subject)
 admin.site.register(ModuleFile)
